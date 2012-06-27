d11a2031413e29b3213b8f868d853b346a660a45
Artur Rodrigues <arturhoo@gmail.com>
2012-06-27 18:21:28 -0300

    refatoracao do solver

--- gerador.py
+++ gerador.py
@@ -1,135 +1,157 @@
 # -*- coding: utf-8 -*-
-import networkx as nx
-import random as rand
-import os
+from solver import solve
 from cPickle import dump
-from datetime import datetime
+from os import path, makedirs
+import argparse as ap
+import random as rand
+import networkx as nx
 
 
-def generate_demands(graph, n, u):
+def gera_demandas(graph, n, u):
     base = graph.number_of_nodes() + 1
     for i in range(base, base + n):
         graph.add_node(i, {'demand': rand.randrange(1, u + 1)})
 
 
-def generate_transships(graph, n):
+def gera_transbordos(graph, n):
     base = graph.number_of_nodes() + 1
     for i in range(base, base + n):
         graph.add_node(i, {'demand': 0})
 
 
-def generate_source(graph):
+def gera_oferta(graph):
     base = graph.number_of_nodes() + 1
-    total_demand = 0
+    demanda_total = 0
     for i_node in graph.nodes():
-        total_demand += graph.node[i_node]['demand']
-    graph.add_node(base, {'demand': - total_demand})
+        demanda_total += graph.node[i_node]['demand']
+    graph.add_node(base, {'demand': - demanda_total})
 
 
-def generate_edges(graph, n, u, c):
-    n_nodes = graph.number_of_nodes()
-    e_added = 0
-    while e_added < n:
-        origin = rand.randrange(1, n_nodes + 1)
-        destiny = rand.randrange(1, n_nodes + 1)
-        while (destiny == origin):
-            destiny = rand.randrange(1, n_nodes + 1)
-        if not graph.has_edge(origin, destiny):
-            e_added += 1
-            cap = rand.randrange(u / 2 + 1, u + 1)  # evita grande dispersão de valores através do lowerbound u / 2 + 1
-            cost = rand.randrange(0, c + 1)
-            graph.add_edge(origin, destiny, \
-                          {'capacity': cap, 'weight': cost, 'fluxo': 0})
+def gera_arcos(graph, n, u, c):
+    n_vertices = graph.number_of_nodes()
+    n_arestas_adicionadas = 0
+    while n_arestas_adicionadas < n:
+        origem = rand.randrange(1, n_vertices + 1)
+        destino = rand.randrange(1, n_vertices + 1)
+        while (destino == origem):
+            destino = rand.randrange(1, n_vertices + 1)
+        if not graph.has_edge(origem, destino):
+            n_arestas_adicionadas += 1
+            cap = rand.randrange(u / 2 + 1, u + 1)  # evita grande dispersão
+                                                    # de valores através do
+                                                    # lowerbound u / 2 + 1
+            custo = rand.randrange(0, c + 1)
+            graph.add_edge(origem, destino, \
+                          {'capacity': cap, 'weight': custo, 'fluxo': 0})
 
 
-def format_output(graph):
-    output = 'set Nodes := '
+def confere_integralidade_parametros(n_dem, n_tr, n_edge,
+                                     u_dem, u_cap, u_cost):
+    min_dem = 1
+    if (n_dem < min_dem):
+        raise Exception('O número de nós de demanda é menor do que o mínimo')
+
+    max_edge = (n_dem + n_tr + 1) * (n_dem + n_tr)  # arranjo dos nós em pares
+    if (n_edge > max_edge):
+        raise Exception('O número de arestas é maior do que o máximo')
+
+    min_u = 1
+    if (u_dem < min_u):
+        raise Exception('''O limite superior para a demanda é
+                        menor do que o mínimo ''')
+
+    min_cap = 1
+    while (u_cap < min_cap):
+        raise Exception('Valor máximo de capacidade menor que o mínimo')
+
+    min_cost = 0
+    while (u_cost < min_cost):
+        raise Exception('Valor máximo de custos menor que o mínimo')
+
+
+def formata_saida_glpk(graph):
+    saida = 'set Nodes := '
     for node in graph.nodes():
-        output += str(node) + ' '
-    output += ';\n'
+        saida += str(node) + ' '
+    saida += ';\n'
 
-    output += 'set Arcs := '
+    saida += 'set Arcs := '
     for edge in graph.edges():
-        output += str(edge) + ' '
-    output += ';\n'
+        saida += str(edge) + ' '
+    saida += ';\n'
 
-    output += '\n\n'
+    saida += '\n\n'
 
-    output += 'param b :=\n'
+    saida += 'param b :=\n'
     for node in graph.nodes():
-        output += str(node) + '\t\t' + str(graph.node[node]['demand']) + '\n'
+        saida += str(node) + '\t\t' + str(graph.node[node]['demand']) + '\n'
 
-    output += ';\n\n'
+    saida += ';\n\n'
 
-    output += 'param:\t\tu\t\tc :=\n'
+    saida += 'param:\t\tu\t\tc :=\n'
     for edge in graph.edges():
-        output += str(edge[0]) + ',' + str(edge[1]) + '\t\t' + \
+        saida += str(edge[0]) + ',' + str(edge[1]) + '\t\t' + \
                   str(graph.edge[edge[0]][edge[1]]['capacity']) + '\t\t' + \
                   str(graph.edge[edge[0]][edge[1]]['weight']) + '\n'
 
-    output += ';\n\nend;\n'
+    saida += ';\n\nend;\n'
 
-    return output
+    return saida
 
+
+def gera_grafo_viavel(n_dem, n_tr, n_edge, u_dem, u_cap, u_cost):
+    graph = nx.DiGraph()
+
+    while(True):
+        try:
+            gera_demandas(graph, n_dem, u_dem)
+            gera_transbordos(graph, n_tr)
+            gera_oferta(graph)
+            gera_arcos(graph, n_edge, u_cap, u_cost)
+            solve(nx.DiGraph(graph))
+        except Exception:
+            graph = nx.DiGraph()
+            continue
+        break
+
+    return graph
 
 if __name__ == '__main__':
-    f = open('input', 'r')
-    n_descr = int(f.readline())  # Número de descritores de instâncias
-    for i in range(0, n_descr):
-        error = ''
+    parser = ap.ArgumentParser(description='Gerador Single Source FCNF')
+    parser.add_argument('n_dem', help='Número de nós de demanda')
+    parser.add_argument('n_tr', help='Número de nós de transbordo')
+    parser.add_argument('dens', help='Densidade do grafo')
+    parser.add_argument('u_dem', help='Valor máximo para demandas')
+    parser.add_argument('u_cap', help='Valor máximo para capacidade nos arcos')
+    parser.add_argument('u_cost', help='Valor máximo para custo nos arcos')
+    args = vars(parser.parse_args())
 
-        n_inst = int(f.readline())  # Número de instâncias geradas para este descritor
+    n_dem = int(args['n_dem'])
+    n_tr = int(args['n_tr'])
+    n_edge = int(float(args['dens']) * (n_dem + n_tr + 1) * (n_dem + n_tr))
+    u_dem = int(args['u_dem'])
+    u_cap = int(args['u_cap'])
+    u_cost = int(args['u_cost'])
+    graph = gera_grafo_viavel(n_dem, n_tr, n_edge,
+                              u_dem, u_cap, u_cost)
 
-        n_dem = int(f.readline())
-        min_dem = 1
-        if (n_dem < min_dem):
-            error += 'O número de nós de demanda é menor do que o mínimo.\n'
+    output = formata_saida_glpk(graph)
+    density = nx.density(graph)
+    size = n_dem + n_tr + 1
+    file_name = 's' + str(size) + \
+                'd' + str(int(round(density, 3) * 100)) + \
+                'u' + str(u_dem)
 
-        n_tr = int(f.readline())
+    glpk_dir = 'instancias/glpk/'
+    if not path.exists(glpk_dir):
+                makedirs(glpk_dir)
+    glpk_out = open(glpk_dir + file_name, 'w')
+    glpk_out.write(output)
+    glpk_out.close()
 
-        n_edge = int(f.readline())
-        max_edge = (n_dem + n_tr + 1) * (n_dem + n_tr)  # arranjo dos nós em pares
-        if (n_edge > max_edge):
-            error += 'O número de arestas é maior do que o máximo.\n'
-
-        u_dem = int(f.readline())
-        min_u = 1
-        if (u_dem < min_u):
-            error += 'O limite superior para a demanda é menor do que o mínimo.\n'
-
-        u_cap = int(f.readline())
-        min_cap = 1
-        while (u_cap < min_cap):
-            error += 'Valor máximo de capacidade menor que o mínimo.\n'
-
-        u_cost = int(f.readline())
-        min_cost = 0
-        while (u_cost < min_cost):
-            error += 'Valor máximo de custos menor que o mínimo.\n'
-
-        for j in range(0, n_inst):
-            graph = nx.DiGraph()
-
-            fmt = '%Y-%m-%d-%H-%M-%S'
-            time_now = datetime.now().strftime(fmt)
-            directory = 'instancias/' + time_now + '/'
-
-            if not os.path.exists(directory):
-                os.makedirs(directory)
-
-            glpk_out = open(directory + 'instancia' + str(i) + str(j), 'w')
-            py_out = open(directory + 'py_instancia' + str(i) + str(j), 'w')
-            if error == '':
-                generate_demands(graph, n_dem, u_dem)
-                generate_transships(graph, n_tr)
-                generate_source(graph)
-                generate_edges(graph, n_edge, u_cap, u_cost)
-                output = format_output(graph)
-            else:
-                output = error
-
-            dump(graph, py_out)
-            glpk_out.write(output)
-            glpk_out.close()
-
-    f.close()
+    py_dir = 'instancias/py/'
+    if not path.exists(py_dir):
+                makedirs(py_dir)
+    py_out = open('instancias/py/py' + file_name, 'w')
+    dump(graph, py_out)
+    py_out.close()
