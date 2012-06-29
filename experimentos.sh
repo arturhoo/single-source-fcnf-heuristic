b431b784d0a78af61234b0d5cd4efec9271b743c
Artur Rodrigues <arturhoo@gmail.com>
2012-06-29 10:11:53 -0300

    adicionando script de experimentos

---
+++ experimentos.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+bold=`tput bold`
+normal=`tput sgr0`
+
+FILES=instancias/glpk/*
+for f in $FILES
+do
+    echo  ${bold}$f${normal}
+    for i in 1 2 3
+    do
+        echo '>> Execução GLPK '$i
+        glpsol --model modelo.mod --data $f -o sol.out | grep -i 'time'
+        cat sol.out | grep -i 'minimum'
+    done
+done
+
+FILES=instancias/py/*
+for f in $FILES
+do
+    echo  ${bold}$f${normal}
+    for i in 1 2 3
+    do
+        echo '>> Execução Python '$i
+        python solver.py -f $f | egrep -i 'tempo|objetivo'
+    done
+done
